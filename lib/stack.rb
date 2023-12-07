7a39884f5e046c79c376dd1aa1b45205ac77abba
John <johnfarris217@gmail.com>
2023-12-06 21:17:19 -0600

    commit tree 7159b1c4d23c2c42f5d5d4e1a505ad9d43335214

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,46 @@
 # your code here
+class Stack
+    def initialize(limit = 1000)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+        if @stack.length < @limit 
+            @stack << value
+        else
+            raise "Stack Overflow"
+        end
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack[-1]
+    end
+
+    def size
+        @stack.length()
+    end
+
+    def empty?
+        @stack.size == 0
+    end
+
+    def full?
+        @stack.size == @limit
+    end
+
+    def search(value)
+        i = @stack.size() - 1
+        while i > -1
+            if @stack[i] == value
+                return @stack.size() - i - 1
+            end
+            i -= 1
+        end
+        -1
+    end
+end
